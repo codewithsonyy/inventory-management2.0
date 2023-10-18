b7ac7278f2c02879e37ece9c9abcf11db4412f5e
Arjun Singh <117923579+aarjun8060@users.noreply.github.com>
2023-10-18 19:02:43 +0530

    feat: Add a delete icon with Api Route of deleting product

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect("mongodb+srv://aarjun8060:Samarpal08%40@cluster0.adxlu1n.mongodb.net/management_github");
   } catch (error) {
     console.log(error);
   }
